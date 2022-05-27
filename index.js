e5454640b0cdcc83baf83b6dcc36e2a50091f4a1
Sujeto47 <ksroa18@ucatolica.edu.co>
2022-05-27 16:07:05 -0500

    Segundo cambio

---
+++ index.js
@@ -0,0 +1,26 @@
+var cadena;
+var tener = RegExp("(TIENE|TIENES|TENES|TENGO|TENÉS)");
+var edad = /AÑOS/;
+function evaluarPregunta(){
+    cadena = document.getElementsByName("txtPregunta")[0].value;
+    cadena = cadena.toUpperCase();
+    document.getElementById("resultado1").innerHTML= tener.test(cadena);
+    //document.getElementById("resultado2").innerHTML= edad.test(cadena);
+    if(tener.test(cadena)==true/*&&edad.test(cadena)==true*/){
+        cod = 4
+        alert (cod);
+    }else{
+        cod =0;
+    }
+    responder();
+}
+function responder(){
+    switch (cod){
+        case 4:
+            mensaje = "Tengo 24 años"
+            break;
+        default:
+        mensaje = "Sin resultados"
+    }
+    document.getElementById("respuesta").innerHTML=mensaje;
+}
